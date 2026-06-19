db71477a57edf3dc723bfef80bad9a89a626ef2a
pranavgalsinge13 <pranavgalsinge13@gmail.com>
2026-06-19 11:52:48 +0530

    javascript

---
+++ JS/index.js
@@ -0,0 +1,9 @@
+console.log("This is Console");
+        alert("This is alert")
+        confirm("This is Confirm")
+        document.writeln("This is new document")
+
+        //Input method
+
+        let a= prompt("Enter your name:")
+        document.writeln("your name is",a)
---
+++ JS/string.js
@@ -0,0 +1,66 @@
+// string ('' , "", '''  ''')
+
+let a="Fortune"
+console.log(a);
+console.log(typeof(a));
+
+let b=a.charAt(3)
+console.log(b);
+
+let c=a.charCodeAt(3)
+console.log(c);
+
+let d=a.endsWith('e')
+console.log(d);
+
+let e=a.startsWith('f')
+console.log(e);
+
+let f=a.includes('z')
+console.log(f);
+
+let g=a.toLocaleUpperCase()
+console.log(g);
+
+let h=a.toLocaleLowerCase()
+console.log(h);
+
+let i=a.concat("Cloud")
+console.log(i);
+
+let j=a.replace("Fortune","Cravita")
+console.log(j)
+
+let k=a.repeat(5)
+console.log(k)
+
+// number method
+// 34 56.78
+
+let ab=34;
+console.log(ab);
+console.log(typeof(ab));
+
+let xy=67.89897699
+console.log(xy);
+console.log(typeof(xy));
+
+let yz=xy.toFixed(2)
+console.log(yz);
+
+let uv=ab.toString()
+console.log(uv);
+console.log(typeof(uv));
+
+// boolean
+
+let m=true
+console.log(typeof(m));
+
+// null
+
+let n=null
+console.log(typeof(n));
+
+let p;
+console.log(p);
