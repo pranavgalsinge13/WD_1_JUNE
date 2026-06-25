6b7bbdb3bcf5d69bcfe1f450ff478d16a36012af
pranavgalsinge13 <pranavgalsinge13@gmail.com>
2026-06-25 11:31:30 +0530

    createmethods.js

---
+++ JS/createmethods.js
@@ -0,0 +1,9 @@
+let body=document.body
+let div=document.createElement("div")
+
+div.style.background="linear-gradient(red,black)"
+div.style.height="90vh"
+div.style.width="100%"
+
+div.innerHTML += '<h1>Welcome to Js</h1>'
+body.appendChild(div)
