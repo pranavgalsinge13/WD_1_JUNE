5ffe909e2b6a9383c61fa4eaf150b554e83275b8
pranavgalsinge13 <pranavgalsinge13@gmail.com>
2026-06-25 11:31:51 +0530

    event.js

---
+++ JS/event.js
@@ -0,0 +1,19 @@
+function myclick(){
+    document.body.style.background="linear-gradient(pink,white)"
+    document.body.style.height="100vh"
+}
+
+function click1(){
+    document.body.style.background="linear-gradient(red,white)"
+    document.body.style.height="100vh"
+}
+
+function down(){
+    document.body.style.background="linear-gradient(yellow,orange,red)"
+    document.body.style.height="100vh"
+}
+
+function up(){
+    document.body.style.background="linear-gradient(pink,purple,black)"
+    document.body.style.height="100vh"
+}
