bc39b48dd37ad276551fb6bd35efaaef1eb285b3
pranavgalsinge13 <pranavgalsinge13@gmail.com>
2026-06-25 11:31:42 +0530

    object.js

---
+++ JS/object.js
@@ -0,0 +1,24 @@
+// object
+// Literals
+let emp={
+    id:101,
+    name:"JAVA",
+    address:"Pune"
+}
+console.log(emp);
+console.table(emp)
+
+// Constructor Function
+
+let stu={
+    id:111,
+    name:"Scholar",
+    address:{
+        s_street:"JM Road",
+        s_area:"Shivaji Nagar",
+        s_city:"Pune"
+    }
+}
+console.log(stu);
+console.table(stu)
+
